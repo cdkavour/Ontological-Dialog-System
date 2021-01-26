d3b22a2d6281edbb20d0afcd40905a3027fe10b5
Sara Ng <sara.blalock.ng@gmail.com>
2021-01-25 18:11:40 -0800

    1/25 issue pushing

--- FrameDMSimple.py
+++ FrameDMSimple.py
@@ -1,12 +1,12 @@
 from DialogFrameSimple import DialogFrameSimple
 from DialogAct import DialogAct
 from DialogActTypes import DialogActTypes
-import math,random
+import math,random,json
 
 class DB:
     def __init__(self,path):
         with open(path) as f:
-            self.data = pd.DataFrame(json.load(f))
+            self.data = json.load(f)
 
 class FrameDMSimple:
 
@@ -15,26 +15,30 @@ class FrameDMSimple:
         # define frame below, for example:
         self.PreviousSlots = {}
         self.DialogFrame = DialogFrameSimple()
-        self.DB = DB('db.txt')
+        self.DB = DB('db.json')
         self.NLG = NLG
+        self.NLU.setDB(self.DB)
+        self.lastDialogAct = None
 
     def execute(self, inputStr):
         # apply the NLU component
+        import pdb;pdb.set_trace()
         currentSemanticFrame = self.NLU.parse(inputStr)
-
-        change = False
-        if self.PreviousSlots != currentSemanticFrame.Slots:
-            change = True
-
+        self.currentSemanticFrame = currentSemanticFrame
         # update the dialog frame with the new information
         self.trackState(currentSemanticFrame)
-
+        change = False
+        if self.PreviousSlots != currentSemanticFrame.Slots and len(self.PreviousSlots.keys()) == len(currentSemanticFrame.Slots.keys()):
+                change = True
+        
         # and decide what to do next
         newDialogAct = self.selectDialogAct()
         newDialogAct.change = change
 
         # then generate some meaningful response
-        response = self.NLG.generate(newDialogAct,self.DialogFrame) 
+        response = self.NLG.generate(newDialogAct)
+        if self.lastDialogAct.complete:
+            self.submit_order()
         return response
 
     def trackState(self, newSemanticFrame):
@@ -46,6 +50,13 @@ class FrameDMSimple:
                 slots.append(newSemanticFrame.Slots[s])
             except KeyError:
                 slots.append(None)
+        # idx of 0=denied pizza,1=dineid the order, 2= else
+        # if 0:
+        #     delete pizza slots
+        # if 1: 
+        #     delete person modality and address
+
+
         self.DialogFrame.update(slots,self.DB)
 
     def selectDialogAct(self):
@@ -54,8 +65,13 @@ class FrameDMSimple:
         # by default, return a Hello dialog act
         dialogAct.DialogActType = DialogActTypes.HELLO
 
+        if len(self.currentSemanticFrame.Slots.keys())>0 and self.PreviousSlots == self.currentSemanticFrame.Slots:
+            # we need to reqalts
+            dialogAct.DialogActType = DialogActTypes.REQALTS
+            dialogAct.slot = self.lastDialogAct.slot
+
         # Order has been grounded; return goodbye diolog act
-        if self.DialogFrame.ground_order == True:
+        elif self.DialogFrame.ground_order == True:
             dialogAct.DialogActType = DialogActTypes.GOODBYE
             dialogAct.complete = True
         
@@ -73,14 +89,14 @@ class FrameDMSimple:
                 dialogAct.slot = "modality"
 
             # Get address if needed
-            elif DialogFrame.modality == "delivery" not self.DialogFrame.address:
+            elif DialogFrame.modality == "delivery" and not self.DialogFrame.address:
                 dialogAct.DialogActType = DialogActTypes.REQUEST
                 dialogAct.slot = "address"
         
             # Ground Order
             else:
                 dialogAct.DialogActType = DialogActTypes.REQUEST
-                dialogAct.slot = "order"
+                dialogAct.slot = (1,self.DialogFrame)
     
         # Pizza not yet grounded
         else:
@@ -89,7 +105,6 @@ class FrameDMSimple:
             if not self.DialogFrame.pizza.type:
                 dialogAct.DialogActType = DialogActTypes.REQUEST
                 dialogAct.slot = "pizza_type"
-
             # Get crust if needed
             elif not self.DialogFrame.pizza.crust:
                 dialogAct.DialogActType = DialogActTypes.REQUEST
@@ -103,9 +118,9 @@ class FrameDMSimple:
             # Ground Pizza
             elif not self.DialogFrame.pizza.size:
                 dialogAct.DialogActType = DialogActTypes.REQUEST
-                dialogAct.slot = "pizza"
-
-
+                dialogAct.slot = (0,self.DialogFrame)
+        # TODO 
+        self.lastDialogAct = dialogAct
         return dialogAct
 
     def submit_order(self):
